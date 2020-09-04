b56e1d16324156c9f03055138861c18240bd4060
David Trombello <dtrombello@gmail.com>
2020-09-04 00:26:48 -0400

    update testimonial model

--- src/app/Models/testimonial.model.ts
+++ src/app/Models/testimonial.model.ts
@@ -1,2 +1,22 @@
 export class Testimonial {
+
+    id:number;
+    firstName: string;
+    lastName: string;
+    city: string;
+    state: string;
+    country: string;
+    testimonial: string;
+    date: Date;
+
+    constructor(id: number, firstName: string, lastName: string, city: string, state: string, country: string, testimonial: string, date: Date){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.testimonial = testimonial;
+        this.date = date;
+    }
 }
