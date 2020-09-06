95f87d20ef49497ab0c7864d89a8341ba44e0be8
David Trombello <dtrombello@gmail.com>
2020-09-06 01:53:49 -0400

    added prayer-request model, update contact and prayer request forms

---
+++ src/app/Models/prayer-request.model.spec.ts
@@ -0,0 +1,7 @@
+import { PrayerRequest } from './prayer-request.model';
+
+describe('PrayerRequest', () => {
+  it('should create an instance', () => {
+    expect(new PrayerRequest()).toBeTruthy();
+  });
+});
